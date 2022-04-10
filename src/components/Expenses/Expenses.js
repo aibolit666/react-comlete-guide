69b593486819ca82541adcf55541ba5b3df93acf
Pavel <pavel.priladyshev@gmail.com>
2022-04-10 19:36:27 +0300

    refactor: change functions to arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -27,5 +27,5 @@ function Expenses(props) {
       />
     </Card>
   );
-}
+};
 export default Expenses;
